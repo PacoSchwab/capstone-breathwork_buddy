6cf16e1a4d11ff3089feb01ad57d8111c18732c4
Paco Schwab <Paco.Schwab@live.de>
2023-04-17 03:28:44 +0200

    fix bug

--- components/TrackingSection/index.js
+++ components/TrackingSection/index.js
@@ -1,10 +1,3 @@
-/* import Counter from "../Counter";
-import * as V from "victory";
-import useLocalStorage from "use-local-storage";
-
 export default function TrackingSection() {
-  const storedTimes = useLocalStorage("storedTimes", []);
-  return;
-  <p>{storedTimes}</p>;
+  return <p>TrackingSection</p>;
 }
- */
--- pages/tracking.js
+++ pages/tracking.js
@@ -3,7 +3,7 @@ import TrackingSection from "../components/TrackingSection";
 export default function TrackingPage() {
   return (
     <>
-      <TrackingSection storedTimes={storedTimes} />
+      <TrackingSection />
     </>
   );
 }
