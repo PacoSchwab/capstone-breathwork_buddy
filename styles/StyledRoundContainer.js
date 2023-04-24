2e4af56b4a0936b389d02c0da4fead6f4dff2ea5
Paco Schwab <Paco.Schwab@live.de>
2023-04-24 16:24:59 +0200

    fix flex

--- styles/StyledRoundContainer.js
+++ styles/StyledRoundContainer.js
@@ -3,13 +3,13 @@ import styled, { css } from "styled-components";
 export const StyledRoundContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
   ${(iceBathing) =>
     iceBathing &&
     css`
       justify-content: flex-start;
       margin-left: 0.5rem;
     `};
+  justify-content: space-between;
   column-gap: 3.5rem;
   width: 100%;
 `;
--- styles/StyledRoundCounter.js
+++ styles/StyledRoundCounter.js
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const StyledRoundCounter = styled.p`
   color: #454545;
   font-size: 2rem;
+  margin: 0 0.5rem;
 `;
