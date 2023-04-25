063dd389d3835dc91fdcb50575a1edc7ff3fd680
Paco Schwab <Paco.Schwab@live.de>
2023-04-25 20:28:03 +0200

    fix margin

--- styles/StyledCounterSection.js
+++ styles/StyledCounterSection.js
@@ -4,5 +4,6 @@ export const StyledCounterSection = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  margin-top: 3rem;
+  margin-top: 6rem;
+  margin-bottom: 3rem;
 `;
