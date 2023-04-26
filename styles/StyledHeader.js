cfff501e83ecf202ab642cb7e67091889fd2ddeb
Paco Schwab <Paco.Schwab@live.de>
2023-04-26 11:36:40 +0200

    fix margin

--- styles/StyledHeader.js
+++ styles/StyledHeader.js
@@ -7,7 +7,7 @@ export const StyledHeader = styled.h1`
   padding: 0.75rem;
   font: var(--font-headline1);
   background-image: linear-gradient(120deg, #f6d365 0%, #fda085 100%);
-  border-radius: 10%;
+  border-radius: 0% 0% 20% 20%;
   color: #454545;
   box-shadow: 5px 10px 7.5px #ffa559;
   text-shadow: 2px 2px 3px #777;
