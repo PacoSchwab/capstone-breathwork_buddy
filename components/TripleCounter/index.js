77fddce562ea26123485f68c0c91863509319d42
Paco Schwab <Paco.Schwab@live.de>
2023-04-27 14:06:46 +0200

    add header link

--- components/TripleCounter/index.js
+++ components/TripleCounter/index.js
@@ -11,6 +11,7 @@ import useStore from "../useStore";
 import TripleCounterButton from "../TripleCounterButton";
 import { StyledRoundContainer } from "../../styles/StyledRoundContainer";
 import { StyledSubHeadline } from "../../styles/StyledSubHeadline";
+import useClearTimer from "../useClearTimer";
 
 export default function TripleCounter({ breathIntervalDelay }) {
   const router = useRouter();
@@ -78,6 +79,16 @@ export default function TripleCounter({ breathIntervalDelay }) {
   const resetTripleCounters = useStore((state) => state.resetTripleCounters);
 
   useClearAudio(stopTripleCounter, stopTripleCounterMusic);
+  useClearTimer(
+    tripleCounterOneIntervalId,
+    tripleCounterTwoIntervalId,
+    tripleCounterThreeIntervalId,
+    switchIsTripleCounterOneActive,
+    switchIsTripleCounterTwoActive,
+    switchIsTripleCounterThreeActive,
+    resetTripleCounters,
+    clearInterval
+  );
 
   useEffect(() => {
     let tripleCounterOneInterval;
--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,5 @@
-import { StyledExerciseBackground } from "../styles/StyledExerciseBackground";
-import { StyledExerciseButton } from "../styles/StyledExerciseButton";
-import { StyledExerciseLinkText } from "../styles/StyledExerciseLinkText";
-import { StyledOverviewSection } from "../styles/StyledOverviewSection";
-import { StyledExerciseLink } from "../styles/StyledExerciseLink";
-import { StyledHeader } from "../styles/StyledHeader";
+import LandingPage from "../components/LandingPage";
 
 export default function HomePage() {
-  return (
-    <>
-      <StyledOverviewSection>
-        <StyledExerciseLink href="/breathflows">
-          <StyledExerciseButton>
-            <StyledExerciseBackground
-              src="/images/purplesmoke.png"
-              alt="Breath Flows"
-            />
-            <StyledExerciseLinkText>Breath Flows</StyledExerciseLinkText>
-          </StyledExerciseButton>
-        </StyledExerciseLink>
-
-        <StyledExerciseLink href="/triplebreathing" second>
-          <StyledExerciseButton>
-            <StyledExerciseBackground
-              src="/images/purplesmoke.png"
-              alt="4-7-8 Breathing"
-            />
-            <StyledExerciseLinkText>4-7-8 Breathing</StyledExerciseLinkText>
-          </StyledExerciseButton>
-        </StyledExerciseLink>
-
-        <StyledExerciseLink href="/icebathing">
-          <StyledExerciseButton>
-            <StyledExerciseBackground
-              src="/images/purplesmoke.png"
-              alt="Ice Bathing"
-            />
-            <StyledExerciseLinkText>Ice Bathing</StyledExerciseLinkText>
-          </StyledExerciseButton>
-        </StyledExerciseLink>
-      </StyledOverviewSection>
-    </>
-  );
+  return <LandingPage />;
 }
