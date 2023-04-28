2b0bb9f7e8849c3707f8b022195dc35af9d02a38
Paco Schwab <Paco.Schwab@live.de>
2023-04-28 13:28:03 +0200

    accessibility head

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,15 @@ import Layout from "../components/Layout";
 import GlobalStyle from "../styles";
 import FabMenu from "../components/FabMenu";
 import Header from "../components/Header";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>globehopper</title>
+        <meta name="description" content="Breathwork Buddy"></meta>
+      </Head>
       <GlobalStyle />
       <Header />
       <Layout>
